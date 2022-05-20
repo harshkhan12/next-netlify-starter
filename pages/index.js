cbc4fba684b0ae671b43ece71d6308df4d8068cd
harshkhan12 <105911690+harshkhan12@users.noreply.github.com>
2022-05-20 14:03:30 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+         <a href="https://abrasivegrit.in/"> Copper slag </a> grit manufacturer, which is created while pyrometallurgical creation of copper from copper metals contains materials like iron, alumina, calcium oxide, silica, and so on For each huge load of metal creation about 2.2 huge loads of slag is produced. Unloading or removal of such immense measures of slag causes ecological and space issues. During the earlier twenty years, endeavors have been made by a few examiners and copper-delivering units everywhere in the world to investigate the conceivable usage of copper slag grit. The agreeable physicomechanical qualities of copper slag can be used to make the items like concrete, fill, weight, abrasive, total, material granules, glass, tiles, and so on separated from recuperating the important metals by different extractive metallurgical heading
         </p>
       </main>
 
